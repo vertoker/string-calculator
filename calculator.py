3a7c7b85a00837904ba0d5a5c0ff4774bf41e39d
vertoker <vertogpro@gmail.com>
2021-03-28 11:37:23 +0500

    commit 28.03 fixed some bugs

--- calculator.py
+++ calculator.py
@@ -2,7 +2,6 @@ import math, re
 
 order_signs = {'+':1, '-':1, '*':2, '/':2, '^':3, '√':3, '!':4}
 pos_types = {'+':0, '-':0, '*':0, '/':0, '^':0, '√':1, '!':2}
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
 all_nums = '.1234567890'
 
 def Factorial(num):
@@ -34,153 +33,157 @@ def Operation(sign, num1, num2):
     return 0
 
 def Calculate(expression, saveconvert2int = True, convert2int = False):
+	if expression == '':
+		return 0
+
     # Format input
-    expression = expression.replace(',', '.')
-    expression = expression.replace(':', '/')
-    expression = expression.replace('**', '^')
+	expression = expression.replace(',', '.')
+	expression = expression.replace(':', '/')
+	expression = expression.replace('**', '^')
 
     # Start params
-    length = len(expression)
-    localNum = ''
-    power = 0
-    targetNum = 0
-    
+	length = len(expression)
+	localNum = ''
+	power = 0
+	targetNum = 0
+
     # Lists
-    nums = []
-    signs = []
-    order = []
-    target = []
-    
+	nums = []
+	signs = []
+	order = []
+	target = []
+
     # Collect data and write lists
-    for i in range(length):
-        s = expression[i]
-        isNum = False
-        if s in all_nums or s in alphabet:
-            localNum += s
-            isNum = True
-        elif s == '-':
-            if len(signs) == 0:
-                if localNum == '':
-                    localNum += s
-                    isNum = True
-                else:
-                    signs.append(s)
-                    order.append(power)
-                    target.append(targetNum)
-                    targetNum += 1
-            elif localNum == '' and signs[-1] != '!':
-                localNum += s
-                isNum = True
-            else:
-                signs.append(s)
-                order.append(power)
-                target.append(targetNum)
-                targetNum += 1
-        elif s in '+*/:^!√':
-            target.append(targetNum)
-            if s in '+*/:^':
-                targetNum += 1
-            if i > 0:
-                if s == '√' and expression[i - 1] in all_nums:
-                    signs.append('*')
-                    order.append(power)
-                    targetNum += 1
-                    target.append(targetNum)
-            signs.append(s)
-            order.append(power)
-            if i + 1 < length:
-                if s == '!' and expression[i + 1] in all_nums:
-                    signs.append('*')
-                    order.append(power)
-                    target.append(targetNum)
-                    targetNum += 1
+	for i in range(length):
+		s = expression[i]
+		isNum = False
+		if s == '-':
+			if len(signs) == 0:
+				if localNum == '':
+					localNum += s
+					isNum = True
+				else:
+					signs.append(s)
+					order.append(power)
+					target.append(targetNum)
+					targetNum += 1
+			elif localNum == '' and signs[-1] != '!':
+				localNum += s
+				isNum = True
+			else:
+				signs.append(s)
+				order.append(power)
+				target.append(targetNum)
+				targetNum += 1
+		elif s in '+*/:^!√':
+			target.append(targetNum)
+			if s in '+*/:^':
+				targetNum += 1
+			if i > 0:
+				if s == '√' and expression[i - 1] in all_nums:
+					signs.append('*')
+					order.append(power)
+					targetNum += 1
+					target.append(targetNum)
+			signs.append(s)
+			order.append(power)
+			if i + 1 < length:
+				if s == '!' and expression[i + 1] in all_nums:
+					signs.append('*')
+					order.append(power)
+					target.append(targetNum)
+					targetNum += 1
+		elif s == '(':
+			if localNum != '':
+				signs.append('*')
+				order.append(power)
+				target.append(targetNum)
+				targetNum += 1
+			power += 1
+		elif s == ')':
+			if i + 1 < length:
+				if expression[i + 1] in all_nums:
+					signs.append('*')
+					order.append(power)
+					target.append(targetNum)
+					targetNum += 1
+			power -= 1
+		else:
+			localNum += s
+			isNum = True
 
-        elif s == '(':
-            if localNum != '':
-                signs.append('*')
-                order.append(power)
-                target.append(targetNum)
-                targetNum += 1
-            power += 1
-        elif s == ')':
-            if i + 1 < length:
-                if expression[i + 1] in all_nums:
-                    signs.append('*')
-                    order.append(power)
-                    target.append(targetNum)
-                    targetNum += 1
-            power -= 1
-        if not isNum and localNum != '' and localNum != '-':
-            nums.append(localNum)
-            localNum = ''
-    if localNum != '' and localNum != '-':
-        nums.append(localNum)
+		if not isNum and localNum != '' and localNum != '-':
+			nums.append(localNum)
+			localNum = ''
+	if localNum != '' and localNum != '-':
+		nums.append(localNum)
 
-    # Clean numbers and find constants
-    for i in range(len(nums)):
-        num = nums[i]
-        multiplier = 1
-        localNum = ''
-        
-        if 'pi' in num:
-            multiplier *= pow(math.pi, len(re.findall('pi', num)))
-            num = num.replace('pi', '?')
-        if 'e' in num:
-            multiplier *= pow(math.e, len(re.findall('e', num)))
-            num = num.replace('e', '?')
-        
-        for n in num:
-            if (n in alphabet or n == '?') and localNum != '':
-                multiplier *= float(localNum)
-                localNum = ''
-            elif n != '?':
-                localNum += n
-        if localNum != '':
-            multiplier *= float(localNum)
-        nums[i] = multiplier
-    
+	# Clean numbers and find constants
+	for i in range(len(nums)):
+		num = nums[i]
+		multiplier = 1
+		localNum = ''
+
+		if 'pi' in num:
+			multiplier *= pow(math.pi, len(re.findall('pi', num)))
+			num = num.replace('pi', '?')
+		if 'e' in num:
+			multiplier *= pow(math.e, len(re.findall('e', num)))
+			num = num.replace('e', '?')
+
+		for n in num:
+			if n in all_nums:
+				localNum += n
+			elif localNum != '':
+				multiplier *= float(localNum)
+				localNum = ''
+
+		if localNum != '':
+			multiplier *= float(localNum)
+		nums[i] = multiplier
+
     # Calculate
-    for x in range(len(signs)):
-        nextID = 0
-        signPower = order_signs[signs[0]]
-        signOrder = order[0]
-        for y in range(1, len(signs)):
-            localPower = order_signs[signs[y]]
-            localOrder = order[y]
-            if signOrder < localOrder or (signOrder == localOrder and signPower < localPower):
-                nextID = y
-                signPower = localPower
-                signOrder = localOrder
-        '''# Logs
-        print(nums)
-        print(signs)
-        print(order)
-        print(target)'''
-        pos_type = pos_types[signs[nextID]]
-        if pos_type == 0:
-            nums[target[nextID]] = Operation(signs[nextID], nums[target[nextID]], nums[target[nextID] + 1])
-            signs.pop(nextID)
-            order.pop(nextID)
-            nums.pop(target[nextID] + 1)
-            last = target.pop(nextID)
-            for i in range(len(target)):
-                if target[i] > last:
-                    target[i] -= 1
-        elif pos_type == 1:# √
-            nums[target[nextID]] = Operation(signs[nextID], 0, nums[target[nextID]])
-            if target[nextID] + 1 < len(nums) and len(signs) < len(nums):
-                nums[target[nextID]] = Operation('*', nums[target[nextID]], nums[target[nextID] + 1])
-                nums.pop(target[nextID] + 1)
-            signs.pop(nextID)
-            order.pop(nextID)
-            last = target.pop(nextID)
-        elif pos_type == 2:
-            nums[target[nextID]] = Operation(signs[nextID], nums[target[nextID]], 0)
-            signs.pop(nextID)
-            order.pop(nextID)
-            last = target.pop(nextID)
-    if (nums[0] % 1 == 0 and saveconvert2int) or convert2int:
-        return int(nums[0])
-    return nums[0]
+	for x in range(len(signs)):
+		nextID = 0
+		signPower = order_signs[signs[0]]
+		signOrder = order[0]
+		for y in range(1, len(signs)):
+			localPower = order_signs[signs[y]]
+			localOrder = order[y]
+			if signOrder < localOrder or (signOrder == localOrder and signPower < localPower):
+				nextID = y
+				signPower = localPower
+				signOrder = localOrder
+		# Logs
+		'''print(nums)
+		print(signs)
+		print(order)
+		print(target)'''
+		pos_type = pos_types[signs[nextID]]
+		if pos_type == 0:
+			nums[target[nextID]] = Operation(signs[nextID], nums[target[nextID]], nums[target[nextID] + 1])
+			signs.pop(nextID)
+			order.pop(nextID)
+			nums.pop(target[nextID] + 1)
+			last = target.pop(nextID)
+			for i in range(len(target)):
+				if target[i] > last:
+					target[i] -= 1
+		elif pos_type == 1:# √
+			nums[target[nextID]] = Operation(signs[nextID], 0, nums[target[nextID]])
+			if target[nextID] + 1 < len(nums) and len(signs) < len(nums):
+				nums[target[nextID]] = Operation('*', nums[target[nextID]], nums[target[nextID] + 1])
+				nums.pop(target[nextID] + 1)
+			signs.pop(nextID)
+			order.pop(nextID)
+			last = target.pop(nextID)
+		elif pos_type == 2:
+			nums[target[nextID]] = Operation(signs[nextID], nums[target[nextID]], 0)
+			signs.pop(nextID)
+			order.pop(nextID)
+			last = target.pop(nextID)
+	if (nums[0] % 1 == 0 and saveconvert2int) or convert2int:
+		return int(nums[0])
+	return nums[0]
 
 print(Calculate(input('Input expression: ')))
