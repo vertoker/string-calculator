729c5c1aa045ca594c5272a1784e29355207e72f
vertoker <vertogpro@gmail.com>
2021-04-08 15:28:32 +0500

    commit 08.04 2

--- example.py
+++ example.py
@@ -1,10 +1,10 @@
 from calculator import *
 
-answer = Calculate(input('Input expression: '), returnWarning = True, returnError = True)
-print('\nAnswer equals: ', str(answer[0]))
+answer, warnings, errors = Calculate(input('Input expression: '), returnWarning = True, returnError = True)
+print('\nAnswer equals: ', str(answer))
 print('\nAll warnings')
-for warning in answer[1]:
+for warning in warnings:
     print(warning)
 print('\nAll errors')
-for error in answer[2]:
+for error in errors:
     print(error)
